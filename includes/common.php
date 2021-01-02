473eb0b01096b10967cfcc5fbbeff4e95cf925dc
devesh0605 <dsaxena_be18@thapar.edu>
2021-01-02 14:22:51 +0530

    configures server

--- includes/common.php
+++ includes/common.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost", "root", "aarti", "lifestylestore")or die($mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "", "lifestylestore",3308)or die($mysqli_error($con));
 session_start();
